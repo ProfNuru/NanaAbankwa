edbb22ad96c083b43ec83a5e384229e89edbe99c
Nurudeen Abdul-Karim <nurudeen.karim2016@gmail.com>
2022-10-24 21:23:00 +0000

    Nana boro's webpage

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img src='/nana.JPG' />
+      <p>Dear colleague,</p>
+
+      <p>
+        I am Owusu Mensah Richard Nana Abankwah 
+        (Owusu Mensah Richard, on the ballot). 
+        I crave your indulgence to present my vision 
+        to steer the affairs of our noble GSU, in the capacity 
+        of president in the fore year 2022/2023.</p>
+
+      <p>My Leadership would based on the following pillars (VOICE):</p>
+      
+      <ol>
+        <li>Vibrancy</li>
+        <li>Opportunities</li>
+        <li>Inclusiveness</li>
+        <li>Constellation</li>
+        <li>Expedition</li>
+      </ol>
+
+      <h3>VIBRANCY</h3>
+
+      <p>As an association, we ought to be vibrant and 
+      influential in many spheres including but not limited to 
+      national and international matters. With the proper structural
+      framework, we can collectively activate individuals’ potentials 
+      needed for the growth and improvement of ourselves and societies. 
+      Therefore, while we pursue academic scholarship, we cannot overlook 
+      basic tenets that may stifle our efforts in the future. Hence, we must
+      make our collective voices audible.</p>
+      
+      <h3>OPPORTUNITIES</h3>
+
+      <p>
+      I look forward to liaising with appropriate quarters to create opportunities for members meritoriously, to enhance both our academic and entrepreneurial pursuits. Further, I believe in strength in unity and collaboration. Therefore, I look forward to organizing our Alumni to be direct associates of GSU to make them reachable by members in an accessible but appropriate manner for assistance, support, and counseling in times of difficulties.
+      </p>
+
+      <h3>INCLUSIVENESS</h3>
+
+      <p>
+      I look forward to redefining inclusive leadership where everyone’s opinions are considered and integrated for the collective good of our association. Everyone will be on board to justify their abilities. It is time for everyone to own the association and be held accountable for its progress and expansion.
+      </p>
+
+      <h3>CONSTELLATION</h3>
+
+      <p>We have common goals and attributes as a union, however, there remains a high level of apathy among members due to individual differences and geographical separation. I will work hard to fortify the regional representative system by my predecessors and also bring in new matching system. In this matching (constellation) system, volunteer  exiting students would be made available to be direct tutors for new students to assist them directly during their arrival and settlement into their respective schools. This would create immediate and reliable support systems for new students.
+      Further, alumni volunteers would also be made available for existing students who would be willing to consult any at will.</p>
+
+      <h3>EXPEDITION</h3>
+
+      <p>Since the launch of GSU on August 5, 2016, it has remained a routinely-silent organization with barely any welfare capacity to support any member or affiliate. My leadership will challenge the status quo and create new paths for capacity building and welfare support systems. The current trends in global and local environment demands pragmatic measures effective enough to mitigate any unforeseen desperations. And that is what my leadership will pursue.</p>
+
+      <p>If you desire these new directions, share in my vision, and are ready to share your opinions, then, I kindly beseech you to cast your vote for me.</p>
+
+      <p>Vote for VOICE; our strength lies in oneness!</p>
+
+      <p style={{fontWeight:'bolder'}}>THANK YOU</p>
+
+      <p>ong live GSU, long live Japan, and May God bless our homeland Ghana.</p>
+
+      <p>OWUSU MENSAH RICHARD NANA ABANKWAH</p>
+
+    </div>
+  )
+}
+
+export default App
